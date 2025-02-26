1b59efae69c94b02ef921e138fc3d990e7f74e79
Sashakrm <sasakramch@gmail.com>
2025-02-26 09:26:10 +0200

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[4];
+            InputData(mas);
+            Array.Reverse(mas);
+            OutputData(mas);
+            
+            Console.Read();
+        }
+        static void InputData(int[] mas)
+        {
+            int i;
+            for (i = 0; i < 4; i++)
+            {
+                Console.WriteLine($"mas[{i}]= ");
+                mas[i] = int.Parse(Console.ReadLine());
+            }
+            
+        }
+  
+        
+        static void OutputData(int [] mas)
+        {
+            foreach(int element in mas)
+            {
+                Console.WriteLine($"{element}\t");
+            }
+        }
+    }
+}
